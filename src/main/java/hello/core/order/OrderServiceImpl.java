918f7a8f24823aa623c190d26a19238a7f21467c
ajm <jeck77@naver.com>
2024-10-16 22:33:05 +0900

    조회 타입이 2개 이상일 떄

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,11 +8,12 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 // RequiredArgsConstructor final 붙은 것의 생성자를 자동으로 만들어줌
-@RequiredArgsConstructor
+// @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -23,11 +24,15 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    @Autowired
-//    public OrderServiceImpl( MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.discountPolicy = discountPolicy;
-//        this.memberRepository = memberRepository;
-//    }
+    // 타입 매칭이 2개 이상일 때
+    // 1. Autowired로 중복 이름으로 에러가 나면 파라미터를 이름으로 수정 해서 피할 수 있다. discountPolicy -> fixDiscountPolicy
+    // 2. Quilifier 추가 구분자를 붙여주는 방법 discountPolicy -> @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy
+    // 3. Primary 우선순위를 지정한다.
+    @Autowired
+    public OrderServiceImpl( MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
